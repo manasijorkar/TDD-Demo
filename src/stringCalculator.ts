cd764052b7da9e2be544984f59e2716e9a6c6a43
Manasi Jorkar <manasi.jorkar@gmail.com>
2024-12-06 03:39:24 -0700

    initial commit for all files: class , test , package , tsconfig , gitignore  files

---
+++ src/stringCalculator.ts
@@ -0,0 +1,4 @@
+export class StringCalculator{   
+    add(numbers: string){
+    }
+}
---
+++ tests/stringCalculator.test.ts
@@ -0,0 +1,15 @@
+import {StringCalculator} from "../src/stringCalculator";
+
+describe("StringCalculator", () => {
+
+    let calculator: StringCalculator;
+  
+    beforeEach(()=>{
+      calculator = new StringCalculator();
+    });
+  
+    it("should return 0 for empty string", ()=>{
+      expect(calculator.add("")).toBe(0);
+    });
+
+});
