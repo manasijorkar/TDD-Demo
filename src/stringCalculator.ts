c75d339d69fbc58469c08cd709639a91af15bfc7
Manasi Jorkar <manasi.jorkar@gmail.com>
2024-12-06 05:48:24 -0700

    test case was failing : for negative numbers: passed by adding logic : of collecting negative numbers

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,8 +2,15 @@ export class StringCalculator{
     public add(numbers: string): number{
 
         console.log("input numbers string start:" + numbers + "input numbers string end");
+        console.log("numbersLength: ", numbers.length);
 
         if(numbers === "") return 0;
+
+        if (numbers.length === 1){
+            const num = parseInt(numbers);
+            if(isNaN(num)) return 0;
+            return num;
+        }
 
         if(numbers.startsWith("//")){
             const [delimiterPart, fetchedNumbers] = numbers.split("\n", 2);
@@ -15,32 +22,25 @@ export class StringCalculator{
             console.log("sum: ", sum);
             return sum;
         }
-
-        if(numbers.includes(",")){
-            let delimiterRegex = new RegExp("[,\n]+");
-            let sum = this.addition(numbers,delimiterRegex);
-            console.log("sum: ", sum);
-            return sum;
-        }
-
-        if (!numbers.includes(",")){
-            const num = parseInt(numbers);
-            if(isNaN(num)) return 0;
-            return num;
-        }
-
-        return 0;
+    
+        let delimiterRegex = new RegExp("[,\n]+");
+        let sum = this.addition(numbers,delimiterRegex);
+        console.log("sum: ", sum);
+        return sum; 
     }
 
     public addition(numbers: string,delimiter: RegExp): number{
         const numArray = numbers.split(delimiter);
         let sum = 0;
+        let negativeNumbers = [];
         for (let i = 0; i < numArray.length; i++){  
             const num = parseInt(numArray[i]);
+            if(num < 0) negativeNumbers.push(num);
             if(!isNaN(num)){
                 sum += num;
             }     
-        }    
+        } 
+        if(negativeNumbers.length > 0) throw new Error("Negative numbers are not allowed: " + negativeNumbers);   
         return sum;
     }
 }
