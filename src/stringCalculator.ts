486590439df6ff784ce5b8c04d3af745eb552e1e
Manasi Jorkar <manasi.jorkar@gmail.com>
2024-12-06 03:59:26 -0700

    test case was failing : for single number test is passed now : added logic for single number handle

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,5 +1,14 @@
 export class StringCalculator{   
-    add(numbers: string){
+    add(numbers: string): number{
+
         if(numbers === "") return 0;
+
+        if (!numbers.includes(",")){
+            const num = parseInt(numbers);
+            if(isNaN(num)) return 0;
+            return num;
+        } 
+
+        return 0;
     }
 }
