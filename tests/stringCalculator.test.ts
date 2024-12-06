bcf5eff11113af62d7ab5a44cea3bd5442816a0a
Manasi Jorkar <manasi.jorkar@gmail.com>
2024-12-06 04:20:28 -0700

    test condition added for : new lines between numbers

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -20,4 +20,7 @@ describe("StringCalculator", () => {
       expect(calculator.add("1,2,3,4,5")).toBe(15);
     });
 
+    it("should return the sum of numbers even in new lines between numbers", ()=>{
+      expect(calculator.add("1\n2,3")).toBe(6);
+    });
 });
