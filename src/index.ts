c1c9a1cf7b15cdcd3b4510b5bff06eac4eff5217
Manasi Jorkar <manasi.jorkar@gmail.com>
2024-12-06 05:18:13 -0700

    test case was failing : different delimiters. refactor logic for different delimiters and conflicting logic with previous test cases, now passed with every test case added index.ts for development purpose.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { StringCalculator } from "./stringCalculator"; 
+
+const calculator = new StringCalculator();
+calculator.add("1\n2,3");
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,26 +1,46 @@
 export class StringCalculator{   
-    add(numbers: string): number{
+    public add(numbers: string): number{
+
+        console.log("input numbers string start:" + numbers + "input numbers string end");
 
         if(numbers === "") return 0;
+
+        if(numbers.startsWith("//")){
+            const [delimiterPart, fetchedNumbers] = numbers.split("\n", 2);
+            let delimiter = delimiterPart.slice(2);
+            console.log("delimiter: ", delimiter);
+            console.log("fetchedNumbers: ", fetchedNumbers);
+            let delimiterRegex = new RegExp(delimiter);
+            let sum = this.addition(fetchedNumbers,delimiterRegex);
+            console.log("sum: ", sum);
+            return sum;
+        }
+
+        if(numbers.includes(",")){
+            let delimiterRegex = new RegExp("[,\n]+");
+            let sum = this.addition(numbers,delimiterRegex);
+            console.log("sum: ", sum);
+            return sum;
+        }
 
         if (!numbers.includes(",")){
             const num = parseInt(numbers);
             if(isNaN(num)) return 0;
             return num;
-        } 
-
-        if(numbers.includes(",")){
-            const numArray = numbers.split(/[,\n]+/);
-            let sum = 0;
-            for (let i = 0; i < numArray.length; i++){  
-                const num = parseInt(numArray[i]);
-                if(!isNaN(num)){
-                    sum += num;
-                }     
-            }
-            return sum;
         }
 
         return 0;
+    }
+
+    public addition(numbers: string,delimiter: RegExp): number{
+        const numArray = numbers.split(delimiter);
+        let sum = 0;
+        for (let i = 0; i < numArray.length; i++){  
+            const num = parseInt(numArray[i]);
+            if(!isNaN(num)){
+                sum += num;
+            }     
+        }    
+        return sum;
     }
 }
